4451ed7cce94b56c489487c095e6e42722e297b9
ZhonX07 <Creeper000011901@outlook.com>
2025-06-04 10:22:35 +0800

    1

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,31 @@
 import DefaultTheme from 'vitepress/theme'
+import giscusTalk from 'vitepress-plugin-comment-with-giscus'
+import { toRefs } from 'vue'
+import { useData, useRoute } from 'vitepress'
 import './styles/no-select.css' // 引入禁用文本选择的样式
 
 export default {
-  ...DefaultTheme,
+  extends: DefaultTheme,
   enhanceApp({ app }) {
     // 注册组件或其他应用级增强
+  },
+  setup() {
+    const { frontmatter } = toRefs(useData());
+    const route = useRoute();
+
+    giscusTalk({
+      repo: 'ZhonX07/zhonxu-blog',
+      repoId: 'R_kgDOOyofNQ',
+      category: 'Announcements',
+      categoryId: 'DIC_kwDOOyofNc4Cqx6A',
+      mapping: 'pathname',
+      inputPosition: 'top',
+      lang: 'zh-CN',
+      lightTheme: 'noborder_dark',
+      darkTheme: 'noborder_dark',
+    }, {
+      frontmatter,
+      route
+    }, true);
   }
 }
