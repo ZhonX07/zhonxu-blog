23c82cafe4c57e2f0395f39bc1bbeec6c8204c3b
ZhonX07 <Creeper000011901@outlook.com>
2025-06-04 10:21:18 +0800

    1

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,8 @@
 import DefaultTheme from 'vitepress/theme'
 import './styles/no-select.css' // 引入禁用文本选择的样式
-import MyLayout from './Layout.vue'
 
 export default {
   ...DefaultTheme,
-  Layout: MyLayout,
   enhanceApp({ app }) {
     // 注册组件或其他应用级增强
   }
